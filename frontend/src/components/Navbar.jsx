a3d256bb2abad66099a9e9cb5accd871ec942996
Emmanuel Murairi <emurairi18@alastudents.org>
2023-07-13 12:57:42 +0400

    combine frontend

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+
+
+function Navbar() {
+  const [navbar, setNavbar] = useState(false)
+  return (
+    <header class="site-navbar mt-3">
+      <div class="container-fluid">
+        <div class="row align-items-center">
+          <div class="site-logo col-6"><a href="index.html">JobData</a></div>
+
+          <nav class="mx-auto site-navigation">
+            <ul class="site-menu js-clone-nav d-none d-xl-block ml-0 pl-0">
+              <li><a href="index.html" class="nav-link active">Home</a></li>
+              <li><a href="#">Our Team</a></li>
+              <li><a href="#">Career</a></li>
+              
+              <li><a href="/contact">Contact Us</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Navbar
