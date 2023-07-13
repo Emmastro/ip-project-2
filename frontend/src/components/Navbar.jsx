bc20265f4c1652a88b987ecfb6f1bcda1124a41e
Deborah606-byte <53974488+Deborah606-byte@users.noreply.github.com>
2023-07-13 19:52:28 +0400

    started working on the about page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -12,7 +12,7 @@ function Navbar() {
           <nav class="mx-auto site-navigation">
             <ul class="site-menu js-clone-nav d-none d-xl-block ml-0 pl-0">
               <li><a href="index.html" class="nav-link active">Home</a></li>
-              <li><a href="#">Our Team</a></li>
+              <li><a href="/About">Our Team</a></li>
               <li><a href="#">Career</a></li>
               
               <li><a href="/contact">Contact Us</a></li>
---
+++ frontend/src/views/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+
+function About (){
+    return (
+        <>
+     <Navbar/>
+        <div className="w-full h-screen bg-[url('/images/contactuspic.jpg')] bg-cover bg-no-repeat bg-fixed">
+
+        </div>
+        <Footer/>
+        </>
+    )
+}
+
+export default About
