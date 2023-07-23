e25a87ecae7f29510a6a069664b05a704999c6de
Virgo Alpha <bensonkingori96@gmail.com>
2023-07-23 15:19:46 +0400

    debug errors

--- frontend/src/components/SalaryHistoryChart.jsx
+++ frontend/src/components/SalaryHistoryChart.jsx
@@ -1,32 +1,62 @@
+// SalaryHistoryChart.js
 import React, { useState, useEffect } from 'react';
 import { Line } from 'react-chartjs-2';
-import jsonFile from  '../cached_response/salary_history_uk.json';
 
 const SalaryHistoryChart = () => {
   const [chartData, setChartData] = useState({});
 
   useEffect(() => {
-    // Process the data from the JSON file into chart-friendly format
-    const labels = Object.keys(jsonFile.month);
-    const data = Object.values(jsonFile.month);
-
-    setChartData({
-      labels,
-      datasets: [
-        {
-          label: 'Average Salary',
-          data,
-          fill: false,
-          borderColor: 'rgba(75,192,192,1)',
-          tension: 0.1,
-        },
-      ],
-    });
+    // Fetch the JSON data from the file
+    fetchSalaryHistoryData();
   }, []);
+
+  const fetchSalaryHistoryData = async () => {
+    try {
+      const response = await fetch('cached_response/salary_history_uk.json');
+      if (!response.ok) {
+        throw new Error('Failed to fetch the salary history data.');
+      }
+      const contentType = response.headers.get('content-type');
+      // if (!contentType || !contentType.includes('application/json')) {
+      //   throw new Error('Response is not valid JSON.');
+      // }
+      const salaryHistoryData = await response.json();
+      console.log(salaryHistoryData, 'salaryHistoryData')
+      console.log(contentType, 'contentType')
+  
+      // Check if the data is valid
+  //     if (!salaryHistoryData || !salaryHistoryData.month) {
+  //       throw new Error('Invalid salary history data format.');
+  //     }
+  
+      // Process the data from the JSON file into chart-friendly format
+      const labels = Object.keys(salaryHistoryData.month);
+      const data = Object.values(salaryHistoryData.month);
+  
+      setChartData({
+        labels,
+        datasets: [
+          {
+            label: 'Average Salary',
+            data,
+            fill: false,
+            borderColor: 'rgba(75,192,192,1)',
+            tension: 0.1,
+          },
+        ],
+      });
+    } catch (error) {
+      console.error('Error fetching salary history data:', error);
+    }
+  };
 
   return (
     <div>
-      <Line data={chartData} />
+      {Object.keys(chartData).length > 0 ? ( // Check if chartData has valid data
+        <Line data={chartData} />
+      ) : (
+        <p>Loading chart data...</p>
+      )}
     </div>
   );
 };
