43857c2195301a5666aa79b0486ba5279ebc034d
Deborah606-byte <53974488+Deborah606-byte@users.noreply.github.com>
2023-08-02 07:54:49 +0400

    save

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,9 @@ import "./App.css";
 import Login from "./views/Login";
 import Home from "./views/Home";
 import APIPage1 from "./views/APIPage1";
-import About from "./views/About.js";
+import JobCategories from "./views/Job_Categories";
+import About from "./views/Authors.js";
+import Authors from "./views/Authors.js";
 import Register from "./views/Register.js";
 import Contact from "./views/Contact";
 import Blog from "./views/Blog";
@@ -16,7 +18,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/APIPage1" element={<APIPage1 />} />
+        <Route path="/jobcategories" element={<JobCategories />} />
         <Route path="/about" element={<About />} />
+        <Route path="/authors" element={<Authors />} />
         <Route path="/register" element={<Register />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/login" element={<Login />} />
