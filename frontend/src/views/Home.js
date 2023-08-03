9e928e5beece31db81f3d6205bcc3003f51f0775
Deborah606-byte <53974488+Deborah606-byte@users.noreply.github.com>
2023-08-03 08:19:03 +0400

    added a button for the blog on home page

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -113,6 +113,29 @@ const Home = () => {
                 How to work effectively and survive in the IT sector
               </p>
             </div>
+            <div className="relative">
+              <a
+                href="/"
+                className="rounded h-15 w-24 p-2 text-[#F4ECC2] text-center bg-gray-900"
+              >
+                Go To Blogs
+              </a>
+              <svg
+                className="absolute top-40% text-[#F4ECC2] w-6 top-[50%] -translate-x-0 translate-y-[-50%] left-14"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-2 h-2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5"
+                />
+              </svg>
+            </div>
           </div>
         </div>
 
