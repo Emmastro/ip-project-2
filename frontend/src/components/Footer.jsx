87c8b260b36f47d5f6ec3a32371b99277c8f08e6
Deborah606-byte <53974488+Deborah606-byte@users.noreply.github.com>
2023-08-03 21:43:39 +0400

    linked the buttons on the home pae

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
       <h1 className='text-xl w-2/3 md:w-full text-center font-medium md:font-normal md:text-3xl'>Want to find out which job is suitable for your skills?</h1>
       <a href="/" className='rounded w-2/5 md:w-1/5 p-2 text-[#F4ECC2] text-center my-6 bg-gray-900'>Get Started</a>
       <hr className="w-2/3 md:w-1/2 mb-6 border-gray-900 mx-auto lg:mb-6"/>
-      <div className='grid grid-cols-2 md:grid-cols-4 gap-8 md:gap-16'>
+      <div className='grid grid-cols-1 md:grid-cols-3 gap-8 md:gap-16'>
         <div>
           <h1 className='font-semibold'>Search Trending</h1>
           <p className='text-gray-600'>Web Design</p>
@@ -19,7 +19,7 @@ const Footer = () => {
         <div>
           <h1 className='font-semibold'>Company</h1>
           <p className='text-gray-600'>Our Team</p>
-          <p className='text-gray-600'>Careers</p>
+          <p className='text-gray-600'>Job Categories</p>
           <p className='text-gray-600'>Reach Out</p>
         </div>
         <div>
@@ -27,35 +27,6 @@ const Footer = () => {
           <p className='text-gray-600'>Tutorials</p>
           <p className='text-gray-600'>Privacy</p>
           <p className='text-gray-600'>Terms of Service</p>
-        </div>
-        <div>
-          <h1 className='font-semibold'>Socials</h1>
-          <div className='flex justify-between'>
-            <img
-              src='/facebook.png'
-              width={20}
-              height={20}
-              className='hover:scale-125 cursor-pointer'
-            />
-            <img
-              src='/twitter.png'
-              width={20}
-              height={20}
-              className='hover:scale-125 cursor-pointer'
-            />
-            <img
-              src='/instagram.png'
-              width={20}
-              height={20}
-              className='hover:scale-125 cursor-pointer'
-            />
-            <img
-              src='/linkedin.png'
-              width={20}
-              height={20}
-              className='hover:scale-125 cursor-pointer'
-            />
-          </div>
         </div>
       </div>
       <div>
